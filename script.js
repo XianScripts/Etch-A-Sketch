327c3a127acc9e40cca3cd60257afbc8681259ea
Xiannomics <christfontanez@gmail.com>
2023-08-13 05:44:53 -0400

    Outputting 16 grids

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 // Create and grab webpage elements
-const div = document.createElement('div');
+// const div = document.createElement('div');
 const divContainer = document.querySelector('#container');
 console.log(divContainer);
 
 let createGrids = function() {
     for (let i = 0; i < 16; i++) {
+        const div = document.createElement('div');
         div.classList.add('grids');
         divContainer.appendChild(div);
     }
