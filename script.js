2f02dee94ff95900e4b2ec5a3cc2009a02ccd727
Xiannomics <christfontanez@gmail.com>
2023-08-13 05:26:48 -0400

    Creating a single stylized Div so far

--- script.js
+++ script.js
@@ -3,3 +3,11 @@
 const div = document.createElement('div');
 const divContainer = document.querySelector('#container');
 console.log(divContainer);
+
+let createGrids = function() {
+    for (let i = 0; i < 16; i++) {
+        div.classList.add('grids');
+        divContainer.appendChild(div);
+    }
+}
+createGrids();
