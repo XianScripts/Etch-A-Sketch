085c123604fba4cc6017f577c80bc36bc8647941
Xiannomics <christfontanez@gmail.com>
2023-08-14 06:54:49 -0400

    Working grid using CSS grid, will continue flexbox

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 
 // Create and grab webpage elements
 // const div = document.createElement('div');
-const divContainer = document.querySelector('#container');
-console.log(divContainer);
+const container = document.querySelector("#container");
 
-let createGrids = function() {
-    for (let i = 0; i < 16; i++) {
-        const div = document.createElement('div');
-        div.classList.add('grids');
-        divContainer.appendChild(div);
-    }
-}
-createGrids();
+makeRows = function(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) { //Makes 256 items in the loop
+        let cell = document.createElement('div');
+        // cell.innerText = (c + 1);
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+
+makeRows(16, 16);
+
+
+
+// Onmousehover
+// this.event.target.style.color = etc
