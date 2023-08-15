53c00febc8da375e6d21d3657f7180e619b9536c
Christian <xian@pop-os.localdomain>
2023-08-15 13:52:07 -0400

    Temp code for button

--- script.js
+++ script.js
@@ -4,8 +4,9 @@
 const divContainer = document.querySelector('#container');
 // console.log(divContainer);
 
+
 let createGrids = function() {
-    for (let i = 0; i < 256; i++) {
+    for (let i = 0; i < 16*16; i++) {
         const div = document.createElement('div');
         div.classList.add('grids');
         divContainer.appendChild(div);
@@ -17,9 +18,21 @@ createGrids();
 // Setup Hover effects for grid items
 let colorGrides = function() {
     divContainer.addEventListener('mouseover', (e) => {
-        console.log(e.target);
+        // console.log(e.target);
         let cDiv = e.target;
         cDiv.style.backgroundColor = 'black';
     });
 }
 colorGrides();
+
+// Button for custom grid size
+let button = document.querySelector('#grid-size');
+console.log(button);
+
+function gridSize() {
+   button.addEventListener('click', () => {
+    let gridNumber = prompt("What size grid would you like? Max 100");
+   });
+
+};
+
