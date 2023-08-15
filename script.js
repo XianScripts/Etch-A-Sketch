253168883c73532690d2dc9aeca88666acc8de95
Christian <xian@pop-os.localdomain>
2023-08-15 08:46:05 -0400

    Working 16x16 grid, centered items.

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ const divContainer = document.querySelector('#container');
 console.log(divContainer);
 
 let createGrids = function() {
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < 256; i++) {
         const div = document.createElement('div');
         div.classList.add('grids');
         divContainer.appendChild(div);
     }
 }
 createGrids();
+
+
+let vGrids = function() {
+
+}
