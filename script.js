cf5ac3398d1b4098e39c2c709d4cf59204979322
Xiannomics <christfontanez@gmail.com>
2023-08-17 17:26:24 -0400

    Working Randomize, and Reset button. Project complete

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // Create and grab webpage elements
 // const div = document.createElement('div');
 const divContainer = document.querySelector('#container');
+const resetButton = document.querySelector('#reset');
+
 // console.log(divContainer);
 
 // Function to create grid
@@ -11,11 +13,13 @@ let createGrids = function(x) {
         div.classList.add('grids');
         divContainer.appendChild(div);
     }
+    
 }
 // Default 16x16 Grid
 createGrids(16);
 
 
+const rgbButton = document.querySelector('#rgb');
 // Setup Hover effects for grid items
 let colorGrids = function() {
     divContainer.addEventListener('mouseover', (e) => {
@@ -23,11 +27,32 @@ let colorGrids = function() {
         let cDiv = e.target;
         cDiv.style.backgroundColor = 'black';
     });
+    rgbButton.addEventListener('click', (e) => {
+        divContainer.addEventListener('mouseover', (e) => {
+            cDiv = e.target;
+            cDiv.style.backgroundColor = getRandomColor();
+        })
+    })
+    resetButton.addEventListener('click', () => {
+        let divGrids = document.querySelectorAll('.grids');
+        for (let i = 0; i < divGrids.length; i++) {
+            divGrids[i].style.backgroundColor = null;
+        }
+        divContainer.addEventListener('mouseover', (e) => {
+            // console.log(e.target);
+            
+            let cDiv = e.target;
+            cDiv.style.backgroundColor = 'black';
+
+        });
+        
+    });
+
 }
 colorGrids();
 
 // Button for custom grid size
-let button = document.querySelector('#grid-size');
+const button = document.querySelector('.button');
 
 // Button function for custom grid size
 function gridSize() {
@@ -36,11 +61,18 @@ function gridSize() {
         divContainer.removeChild(divContainer.firstChild);
     }
     
-    let gridNumber = prompt("What size grid would you like? Max 100");
+    let gridNumber = prompt("What size grid would you like? Max 100, Default min 16");
     if (gridNumber === "null" || null || "") {
         divContainer.style.width = "850px";
         createGrids(gridNumber);
 
+    }
+    else if (gridNumber <= 16) {
+        divContainer.style.width = "850px";
+        createGrids(16);
+    }
+    else if (gridNumber > 16 && gridNumber < 25) {
+        createGrids(gridNumber);
     }
     else if (gridNumber >= 25 && gridNumber < 50) {
         divContainer.style.width = "1000px";
@@ -61,3 +93,17 @@ function gridSize() {
 
 };
 gridSize();
+
+
+
+// RGB function
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
